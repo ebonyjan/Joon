1c61e939ed847323d0f2601cba47ed32c3339d81
Ebony Preciado <96275501+ebonyjan@users.noreply.github.com>
2023-07-17 19:51:49 -0600

    Update joonjs.js

---
+++ javascript/joonjs.js
@@ -0,0 +1,14 @@
+const items = document.querySelectorAll('.item')
+const options = {
+  threshold: 0.5
+}
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('slide-in');
+    }
+  });
+}, options)
+items.forEach(item => {
+  observer.observe(item);
+})
